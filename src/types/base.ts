245aaff7184028288f48c71a31fb404c75c7fd25
maanex <andreas@maanex.me>
2021-09-11 13:20:25 +0200

    fix typings again

--- src/types/base.ts
+++ src/types/base.ts
@@ -194,7 +194,7 @@ export type RichMessageInteraction = InteractionBase & (InteractionLocationGuild
 export type SlotedContext = { params: Record<string, string> }
 
 export type ReplyableCommandInteraction = CommandInteraction & {
-  defer(privately: boolean): void
+  defer(privately?: boolean): void
   reply(data: InteractionApplicationCommandCallbackData): void
   replyInteractive(data: InteractionApplicationCommandCallbackData): InteractionReplyStateLevelTwo
   replyPrivately(data: InteractionApplicationCommandCallbackData): void
