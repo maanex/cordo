dbe050c8a054485b0f464e5a45ef8ba815c62784
Andreas <andreas@maanex.me>
2025-04-21 12:18:56 +0200

    add examples in test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
-import express, { type Request, type Response } from 'express'
-import { verifyKeyMiddleware } from "discord-interactions"
-import type { APIInteraction } from 'discord-api-types/v10'
+import express from 'express'
 import { Cordo } from '../src/core'
+import { useWithExpress } from '../src/plugin'
 
 
 await Cordo.mountCordo()
@@ -11,15 +10,19 @@ await Cordo.mountCordo()
 const app = express()
 app.set('trust proxy', 1)
 const clientPublicKey = 'ae92419b8a8752234363888abdc0d2b7bdfc730b4b9182dd6c1d5af594842f12'
-const checkKey = verifyKeyMiddleware(clientPublicKey)
-app.post('/', (req: Request, res: Response) => {
-  checkKey(req, res, () => {
-    const interaction = req.body as APIInteraction
-    // interaction._httpCallback = (payload: any) => res.status(200).json(payload)
-    Cordo.triggerInteraction(interaction, {
-      httpCallback: (payload: any) => res.status(200).json(payload)
-    })
-  })
-})
+app.use(useWithExpress(clientPublicKey))
 app.listen(5058, () => console.log('Cordo is running on port 5522'))
 
+/* OR WITH HONO
+
+const app = new Hono()
+app.use('/cordo', useWithHono(clientPublicKey))
+return app
+*/
+
+/* OR WITH DISCORD.JS
+
+const client = new Client({ ... })
+useWithDiscordJs(client)
+client.login()
+*/
