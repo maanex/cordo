035e2bf76b09d3ee347d41492309ccff200b91e0
Andreas <git@maanex.me>
2026-08-06 17:58:23 +0200

    expose cordo/functions Extends

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,13 @@
+import { FunctCompiler } from './compiler'
+
 export { goto } from './impl/goto'
 export { run } from './impl/run'
 export { value } from './impl/value'
 
 export type { CordoFunct, CordoFunctRun } from './funct'
+
+
+export namespace Extend {
+  export const toCustomId: typeof FunctCompiler.toCustomId = (...args) => FunctCompiler.toCustomId(...args)
+  export const parseCustomId: typeof FunctCompiler.parseCustomId = (...args) => FunctCompiler.parseCustomId(...args)
+}
